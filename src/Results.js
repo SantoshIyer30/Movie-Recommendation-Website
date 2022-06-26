bfd6dcfbfb3d1d27bf7a9887b4c682fc55f3c923
K Santosh Iyer <70837375+SantoshIyer30@users.noreply.github.com>
2022-06-26 13:51:49 +0530

    Add files via upload
    Results' Main content added

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import Card from "./Cards";
+import Main from "./Main";
 
 
 function Results(){
     return(
         <Container>
+            <Main/>
             <Card 
                 id='1'
                 title='iron man'
                 image='/assets/images/1154.jpg'
             
             />
+            <BgImage />
         </Container>
 
 
@@ -40,6 +43,21 @@ function Results(){
 
 const Container=styled.section`
     padding-top: 70px ;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image:url('/assets/images/background-homepage.jpg');
+  background-color: rgbargba(0,0,0,0.5);
+  position: absolute;
+  top:0;
+  right:0;
+  left:0;
+  z-index:-1;
+  opacity: 1;
 `;
 
 
